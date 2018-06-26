e0468717102308fe8336a55f3de497e5cec2c14e
Alexander Seipel <a.seipel@immowelt.de>
2018-06-26 10:03:22 +0200

    Another release bugfix

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(name='mongoprocessing',
-      version='0.3.0',
+      version='0.4.0',
       description='A simple library for developing workflows using MongoDB Change Streams.',
-      long_description=long_description,
       long_description_content_type="text/markdown",
       url='https://github.com/Immowelt/MongoProcessing',
-      download_url='https://github.com/Immowelt/MongoProcessing/archive/0.3.0.tar.gz',
+      download_url='https://github.com/Immowelt/MongoProcessing/archive/0.4.0.tar.gz',
       keywords='mongodb changestream changestreams pipeline workflow',
       author='Immowelt AG',
       author_email='a.seipel@immowelt.de',
