0d52b1dee34f34c34c72a2e94cd0ca75324bc6bf
Maxim Fridental <mfridental@mfridentalub18.immoweltag.de>
2019-05-17 14:33:28 +0200

    Fix MultipleDependency, again

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='mongoprocessing',
-      version='0.5.1',
+      version='0.5.2',
       description='A simple library for developing workflows using MongoDB Change Streams.',
       long_description_content_type="text/markdown",
       url='https://github.com/Immowelt/MongoProcessing',
-      download_url='https://github.com/Immowelt/MongoProcessing/archive/0.5.1.tar.gz',
+      download_url='https://github.com/Immowelt/MongoProcessing/archive/0.5.2.tar.gz',
       keywords='mongodb changestream changestreams pipeline workflow',
       author='Immowelt AG',
       author_email='a.seipel@immowelt.de',
